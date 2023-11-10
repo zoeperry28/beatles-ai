5696cc455f2e6da5e0bf4f756bd3c0f67854ba1b
Zoe Perry <42933445+zoeperry28@users.noreply.github.com>
2023-11-10 23:22:24 +0000

    Process larger batches of songs - WIP

--- Data/readme.txt
+++ Data/readme.txt
@@ -1,1 +1,3 @@
 The data which is preprocessed and generated is stored here. You will have to generate this data yourself, but i have left a script in training_data which will be useful for generating your own data.
+
+I'd reccomend sourcing .flac files for the best results.
--- Data/training_data/main.py
+++ Data/training_data/main.py
@@ -1,10 +1,58 @@
 import os
 import shutil
+import logging
+import threading
+import spleeter
+
+COMMAND_LIMIT = 8000 # max is 8191, rounded down to 8000 to give some "wriggle room"
 
 compatible_ext = [".wav", ".flac", ".mp3"]
 
+_format = "%(asctime)s: %(message)s" 
+
+def consolidate_audio(audios):
+    al = []
+    cur = []
+    char_count = 0 
+    index = 0
+    temp = 0
+    for item in audios: 
+        temp = char_count + len(item) + 1
+        if (temp > COMMAND_LIMIT):
+            index = index + 1
+            char_count = 0
+            al.append(cur)
+            cur = []
+            
+        temp = char_count + len(item) + 1
+        char_count = temp
+        cur.append(item)
+    return al
+
+def oconsolidate_audio(audios):
+    char_count = 0 
+    v = " ".join(audios)
+    return v
+
+def group_files(files=[], min_threads=2, max_threads = 10):
+    div = []
+    for i in range(min_threads, max_threads):
+        r = (len(files) / i)
+        if (r != 0 and int(r) != 0):
+            if (r % int(r) == 0):
+                div.append(r)
+    if (div != []):
+        res = int(max(div)) 
+    else: 
+        res = len(files)
+    return (len(files) / int(res)), int(res);
+
 def run_spleeter(audio, loc):
-    os.system("spleeter separate -o \"" + loc + "\" -p spleeter:4stems \"" + audio + "\"")
+    x = consolidate_audio(audio)
+    for y in x:
+        cm = "spleeter separate -o \"" + loc + "\" -p spleeter:4stems " + " ".join(y)
+        os.system(cm)
+    print("ok")
     
 def vocal_file_name_gen(audio):
     ind = max(audio.rfind("\\"), audio.rfind("\\\\"),audio.rfind("/"))+1
@@ -16,25 +64,20 @@ def vocal_file_name_gen(audio):
 def song_name_from_file(file):
     return file[ max(file.rfind("\\"), file.rfind("\\\\"),file.rfind("/"))+1:file.rfind(".")]
 
-def get_audio(audio, nfile="NULL", loc="TEMP"):
-    if (nfile == "NULL"):
-        nfile = vocal_file_name_gen(audio)
-    run_spleeter(audio, loc)
+def get_audio(audios, loc="TEMP"):
+    if (os.path.exists(loc) == False):
+        os.mkdir(loc)
 
-    new_folder = song_name_from_file(audio)
-    shutil.move(loc + "/"+ new_folder + "/vocals.wav", loc + "/" + nfile)
-    shutil.rmtree(loc + "/"+ new_folder)
+    #nfile = vocal_file_name_gen(audios)
+    run_spleeter(audios, loc)
+
+    #new_folder = song_name_from_file(audio)
+    #
+    #a = loc + "/"+ new_folder.strip() + "/vocals.wav"
+    #b = loc + "/" + nfile
+    #shutil.move(a, b)
+    #shutil.rmtree(loc + "/"+ new_folder)
     return -1
-
-def get_audio_in_folder(folder, recursive=False):
-    found = []
-    if (recursive == True):
-        for root, dirs, files in os.walk(folder, topdown=False):
-            for name in files:
-                found.append(os.path.join(root, name))
-    else: 
-        found = os.listdir(folder)
-    return found
 
 def check_file_extension(file):
     to_return = False;
@@ -44,12 +87,22 @@ def check_file_extension(file):
         to_return = True
     return to_return
 
-def check_all_files(folder, recursive = False) : 
-    files = get_audio_in_folder(folder, recursive)
-    length = len(files)
-    for file in files:
-        res = check_file_extension(file)
-        if (res == True):
-            get_audio(file)
+def get_audio_in_folder(folder, recursive=False):
+    found = []
+    if (recursive == True):
+        for root, dirs, files in os.walk(folder, topdown=False):
+            for name in files:
+                current = os.path.join(root, name)
+                if (check_file_extension(current) == True):
+                    found.append("\"" + current + "\"")
+    else: 
+        found = os.listdir(folder)
+    return found
 
-check_all_files("C:\\Users\\zoepe\\Music\\Unreleased", True)
+def check_all_files(fo, result_path, recursive = False) : 
+    
+    files = get_audio_in_folder(fo, recursive)
+    get_audio(files, result_path)
+        
+check_all_files("C:\\Users\\zoepe\\Music\\Beatles\\George Harrison", "data\george", True)
+#check_all_files("D:\\zoepe\\Music\\Beatles\\John Lennon", "data\john", True)
