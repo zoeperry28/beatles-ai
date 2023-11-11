01d017d7a436bb25a8e803a7b0d00e5ca2149c98
Zoe Perry <42933445+zoeperry28@users.noreply.github.com>
2023-11-11 15:46:44 +0000

    Modified to use spleeter functions, improving speeds

--- Data/training_data/main.py
+++ Data/training_data/main.py
@@ -2,7 +2,11 @@ import os
 import shutil
 import logging
 import threading
-import spleeter
+from spleeter.separator import Separator
+import tensorflow as tf
+import warnings
+from pathlib import Path
+import concurrent 
 
 COMMAND_LIMIT = 8000 # max is 8191, rounded down to 8000 to give some "wriggle room"
 
@@ -10,6 +14,67 @@ compatible_ext = [".wav", ".flac", ".mp3"]
 
 _format = "%(asctime)s: %(message)s" 
 
+def chunks(arr, chunks):
+    temp = []
+    al = []
+    for item in arr: 
+        if (len(temp) == chunks):
+            al.append(temp)
+            temp = []
+        temp.append(item)
+    al.append(temp)    
+    return al
+
+def group_files(files=[], min_threads=2, max_threads = 10):
+    div = []
+    for i in range(min_threads, max_threads):
+        r = (len(files) / i)
+        if (r != 0 and int(r) != 0):
+            if (r % int(r) == 0):
+                div.append(r)
+    if (div != []):
+        res = int(min(div)) 
+    else: 
+        res = len(files)
+    return int(len(files) / int(res)), int(res);
+
+def vocal_file_name_gen(audio):
+    ind = max(audio.rfind("\\"), audio.rfind("\\\\"),audio.rfind("/"))+1
+    v = audio[ind:].lower().replace(" ", "_")
+    a = v.rfind(".")
+    out =  (v[:a].replace(".", "") + "-vocal" + v[a:]).replace(".flac", ".wav")
+    return out
+
+def modify_file(filename, dir):
+    to_return = "";
+    o = filename.split("\\")
+    p = o[len(o)-1]
+    p = p[:p.rfind(".")]
+
+    fn = dir + "\\" + p + "\\vocals.wav"
+    
+    to_return = dir + "\\" + vocal_file_name_gen(filename)
+    shutil.move(fn, to_return)
+
+    shutil.rmtree(dir + "\\" + p)
+    return to_return
+
+def r(input_dir, output_dir):
+    # Create a Spleeter separator
+    separator = Separator('spleeter:4stems')
+
+    # Iterate through each file in the input directory
+    for filename in input_dir:
+        if filename.endswith(".flac") or filename.endswith(".mp3"):
+            
+            # Create output directory if it doesn't exist
+            os.makedirs(output_dir, exist_ok=True)
+
+            # Use Spleeter to separate the audio
+            o = separator.separate_to_file(filename, output_dir)
+            
+            modify_file(filename, output_dir)
+            
 def consolidate_audio(audios):
     al = []
     cur = []
@@ -34,18 +99,7 @@ def oconsolidate_audio(audios):
     v = " ".join(audios)
     return v
 
-def group_files(files=[], min_threads=2, max_threads = 10):
-    div = []
-    for i in range(min_threads, max_threads):
-        r = (len(files) / i)
-        if (r != 0 and int(r) != 0):
-            if (r % int(r) == 0):
-                div.append(r)
-    if (div != []):
-        res = int(max(div)) 
-    else: 
-        res = len(files)
-    return (len(files) / int(res)), int(res);
+
 
 def run_spleeter(audio, loc):
     x = consolidate_audio(audio)
@@ -54,12 +108,6 @@ def run_spleeter(audio, loc):
         os.system(cm)
     print("ok")
     
-def vocal_file_name_gen(audio):
-    ind = max(audio.rfind("\\"), audio.rfind("\\\\"),audio.rfind("/"))+1
-    v = audio[ind:].lower().replace(" ", "-")
-    a = v.rfind(".")
-    out =  v[:a].replace(".", "") + "-vocal" + v[a:];
-    return out
 
 def song_name_from_file(file):
     return file[ max(file.rfind("\\"), file.rfind("\\\\"),file.rfind("/"))+1:file.rfind(".")]
@@ -69,7 +117,7 @@ def get_audio(audios, loc="TEMP"):
         os.mkdir(loc)
 
     #nfile = vocal_file_name_gen(audios)
-    run_spleeter(audios, loc)
+    r(audios, loc)
 
     #new_folder = song_name_from_file(audio)
     #
@@ -94,7 +142,7 @@ def get_audio_in_folder(folder, recursive=False):
             for name in files:
                 current = os.path.join(root, name)
                 if (check_file_extension(current) == True):
-                    found.append("\"" + current + "\"")
+                    found.append(current)
     else: 
         found = os.listdir(folder)
     return found
@@ -104,5 +152,11 @@ def check_all_files(fo, result_path, recursive = False) :
     files = get_audio_in_folder(fo, recursive)
     get_audio(files, result_path)
         
-check_all_files("C:\\Users\\zoepe\\Music\\Beatles\\George Harrison", "data\george", True)
+#check_all_files("C:\\Users\\zoepe\\Music\\Beatles\\George Harrison", "data\george", True)
 #check_all_files("D:\\zoepe\\Music\\Beatles\\John Lennon", "data\john", True)
+
+
+if __name__ == '__main__':
+    warnings.filterwarnings('ignore')
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    check_all_files("C:\\Users\\zoepe\\Music\\Beatles\\George Harrison", "data\george", True)
